95cd5d7704efc44271e7fba003f0371584ca189a
Jeong04 <68769953+Jeong04@users.noreply.github.com>
2023-03-09 20:47:31 +0900

    Create WhereIsDot.java

---
+++ LV0/WhereIsDot.java
@@ -0,0 +1,25 @@
+public class WhereIsDot {
+
+	public static void main(String[] args) {
+		Dot d = new Dot();
+		
+		int[] num = {-7, 9};
+		
+		System.out.println(d.solution(num));
+	}
+}
+
+class Dot {
+	public int solution(int[] dot) {
+		int answer = 0;
+		
+		for(int i = 0; i < dot.length; i++) {
+			if(dot[0] > 0 && dot[1] > 0) { answer = 1; }
+			else if(dot[0] < 0 && dot[1] > 0) { answer = 2; }
+			else if(dot[0] < 0 && dot[1] < 0) { answer = 3; }
+			else if(dot[0] > 0 && dot[1] < 0) { answer = 4; }
+		}
+		
+		return answer;
+	}
+}
