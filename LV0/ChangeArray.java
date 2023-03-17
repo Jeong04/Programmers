b96c08f3a3fc8028ac88269dcd0ec7e2021ccddc
Jeong04 <68769953+Jeong04@users.noreply.github.com>
2023-03-17 16:06:27 +0900

    Create ChangeArray.java

---
+++ LV0/ChangeArray.java
@@ -0,0 +1,15 @@
+public class ChangeArray {
+
+  public static void main(String[] args) {
+    Solution5 s5 = new Solution5();
+    System.out.println(s5.solution(null));
+  }
+}
+
+class Solution5 {
+
+  public int[] solution(int[] num_list) {
+    int[] answer = {};
+    return answer;
+  }
+}
